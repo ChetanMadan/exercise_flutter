f1f268e17e9bfe41bf338bcdb14ec6a3e46a743f
Isha <ishagar98@gmail.com>
2018-12-14 16:49:28 +0530

    hello

--- lib/pages/exercise.dart
+++ lib/pages/exercise.dart
@@ -7,6 +7,33 @@ class Exercise extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Exercise'),
       ),
+      body: ListView.builder(
+          itemCount: 6,
+          itemBuilder: (BuildContext context,int index){
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  contentPadding: EdgeInsets.all(10.0),
+                  leading: Container(
+                    padding: EdgeInsets.all(3.0),
+                    decoration: new BoxDecoration(
+                      color: Color(0xFF9733ee),
+                      shape: BoxShape.circle
+                    ),
+                    child: CircleAvatar(
+                      backgroundImage: new AssetImage('images/google.jpg'),
+                      radius: 36.0,
+                    ),
+                  ),
+                  title: new Text("DAY $index"),
+
+
+                ),
+                Divider(height: 20.0,)
+              ],
+            );
+          }
+      ),
     );
   }
 }
